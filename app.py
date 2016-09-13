5bf753eec71a2fb6e7154dca2090a0e7dd224b56
jehuston <huston.jane@gmail.com>
2016-09-13 16:56:18 -0700

    added DIY page

--- app.py
+++ app.py
@@ -21,5 +21,21 @@ def predict():
     prediction = model.predict(X)[0]
     return render_template('predict.html', prediction = prediction, title = 'Results')
 
+
+'''
+Try it:
+Create a new .html file in the templates folder. Feel free to copy a fancy template
+from the internet or just add some really simple html. Note this is a super
+simple app without external stylesheets so add any CSS in the head or inline.
+(if that makes no sense, then don't worry about it-- keep it simple!)
+
+Edit the function below to add your page to the app.
+'''
+
+@app.route('/your-url-address-here')
+def new_page():
+    # if you copy one of the existing html files in the folder, you'll also need to add a title argument below
+    return render_template('your-html-filename.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
